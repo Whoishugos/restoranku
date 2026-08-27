aefcbe94404b5a55f0817c916a305dc39781cbb5
Whoishugos <tommyonair22@gmail.com>
2026-08-27 19:50:36 +0800

    trouble admin dashboard

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+
+    public function definition()
+{
+    return [
+        'name' => $this->faker->word,
+        'category_id' => Category::inRandomOrder()->first()->id, // Ambil ID kategori yang valid
+        'price' => $this->faker->numberBetween(1000, 10000),
+        'description' => $this->faker->sentence,
+        'img' => $this->faker->imageUrl(),
+        'is_active' => $this->faker->boolean,
+        ];
+    }
+}
